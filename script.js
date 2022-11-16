b8d2dcc4144ead553f7eae5aafd8fab4a5c05b2c
AnukaJapa <japaridze.ana96@gmail.com>
2022-11-16 14:30:33 +0100

    fatto funzione di palidrama

--- script.js
+++ script.js
@@ -2,14 +2,20 @@
 //*Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma e poi usate quella funzione appena dichiarata
 
 function isPalidrama(word){
-
+if(word==reverseWord(word)){
+    console.log("questa parola è Palidrama");
+    return true
+} else {
+    console.log("questa parola non è Palidrama")
+    return false
+}
 }
 
 function reverseWord(word){
     let reversedWord = "";
-    for(let i=word.length-1; i>=0; i++){
+    for(let i = word.length-1; i>=0; i--){
         reversedWord += word[i];
        }
-       console.log(reversedWord);
+       //console.log(reversedWord);
        return reversedWord;
 }
